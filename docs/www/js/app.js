d4cb2aa935a8d735e47327bd6065861332153fb8
JerryWiltz <jerry.wiltz@gmail.com>
2018-01-30 15:26:16 -0800

    Hosting with github

--- docs/www/js/app.js
+++ docs/www/js/app.js
@@ -2,7 +2,7 @@
 
 requirejs.config({
     //By default load any module IDs from js/lib
-    baseUrl: 'js',
+    baseUrl: 'www/js',
     //except, if the module ID starts with "app",
     //load it from the js/app directory. paths
     //config is relative to the baseUrl, and
