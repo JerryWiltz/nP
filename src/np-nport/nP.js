1ffc52fbbe7f9093f99a5d30c4d9f91008779b25
JerryWiltz <jerry.wiltz@gmail.com>
2018-07-31 16:21:52 -0700

    I got 3 series 75/3 resistors to equal 75 with nP.nodal!

--- src/np-nport/nP.js
+++ src/np-nport/nP.js
@@ -510,7 +510,7 @@
 	}
 
 	function nodal( ... nPortsAndNodes) { //nPortsAndNodes = [[nPort1, n1, n2 ...], [nPort2, n1, n2 ...], ... ['out', n1, nn2, ...] ]
-		var i = 0;
+		var i = 0, j = 0, k = 0, row = 0, col = 0, offset = 0, base = 0;
 		var spars = function () { // creates spars table with frequencies only [ [freq1], [freq2], ... [freqN] ]
 			var sparsLength = nPortsAndNodes[0][0].global.fList.length; // use the first nPort for global data
 			var sparsArray = dim(sparsLength,1,1);
@@ -523,22 +523,53 @@
 		var numOfnPorts = nPortsAndNodes.length;
 		var rowCol = function (nPortsAndNodes) { //determine the number of rows and columns
 			var size = 0;
-			for (i = 0; i < numOfnPorts - 1; i++) { 
-				size += Math.sqrt(nPortsAndNodes[i][0].spars[0].length -1);
+			for (i = 0; i < numOfnPorts; i++) { 
+				//size += Math.sqrt(nPortsAndNodes[i][0].spars[0].length -1);
+				size += nPortsAndNodes[i].length -1;
 			}
-			return size + nPortsAndNodes[numOfnPorts-1].length - 1;
+			//return size + nPortsAndNodes[numOfnPorts-1].length - 1;
+			return size;
 		}(nPortsAndNodes);	
 		var zeroArray = function () { return dim(rowCol, rowCol, complex(0,0)); }();
+		var gammaArray = function () {
+			var outArray = dim(rowCol, rowCol, complex(0,0));
+			var outArrayReal = dim(rowCol, rowCol, 0); // for testing hookup
+			var expanded = dim(rowCol, 3, 0);
+			for (row = 0; row < rowCol; row++) {//put the b's here in the first column 
+				expanded[row][0] = row + 1;
+			}		for(i = 0, offset = 0; i < numOfnPorts; i++) {//put the nodes here in the second column
+				for( col = 0; col < nPortsAndNodes[i].length -1; col++) {
+					expanded[offset][1] = nPortsAndNodes[i][col + 1];
+					offset++;
+				}		}		for (i = 0; i < rowCol; i++) {
+				for (row = 0; row < rowCol; row++) { // put the a's in the 3rd column
+					if ( !(i === row) && (expanded[i][1] === expanded[row][1])   ) { //pivot row is not counted
+						expanded[row][2] = expanded[i][0];
+					}			}		}		for (row = 0; row < rowCol; row++) { // put 1's for the interconnects
+				outArray[row][expanded[row][2]-1] = complex(1,0);
+				outArrayReal[row][expanded[row][2]-1] = 1;
+			}		return outArray;
+		}();
 		var abcArray = function () { return dim(rowCol, 1, complex(0,0)); }();
 		var aMatrix = matrix(abcArray);
 		var bMatrix = matrix(abcArray);
 		var cMatrix = matrix(abcArray);
-		var gammaMatrix = matrix(zeroArray);
+		var gammaMatrix = matrix(gammaArray);
 		var nodalOut = new nPort();
-		for ( i = 0; i < numOfnPorts -1; i++) {
-		}	nodalOut.setspars(spars);
+		for ( i = 0; i < 1; i++) { // i is number of frequencies
+			offset = 0;
+			gammaMatrix.m = gammaArray;
+			for ( j = 0; j < nPortsAndNodes.length - 1; j++) { // j is the number of the current nPort except the last one
+				for ( k = 0; k < (nPortsAndNodes[j].length - 1)**2; k++){ // k is the the port number squared
+					base = nPortsAndNodes[j].length - 1;
+					gammaMatrix.m[offset + Math.floor(k/base)][offset + k % base] = nPortsAndNodes[j][0].spars[i][1 + k].neg();
+				}
+				offset += base;
+			}
+		}	gammaMatrix = gammaMatrix.invertCplx();
+		nodalOut.setspars(spars);
 		nodalOut.setglobal(nPortsAndNodes[0][0].global); // use the first nPort for global data
-		return nodalOut;
+		return gammaMatrix;
 	}/*
 	//nodal ... this one takes nPorts and cascades nodally, this is the BIG ONE!!
 			var nodal = function nodal() {           //this moves all the nports into the nodal function
