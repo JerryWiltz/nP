684124f78aa739d599922330d19df1a55f040b0f
JerryWiltz <jerry.wiltz@gmail.com>
2018-08-14 16:10:37 -0700

    Added updated README and added the Rat Race

--- src/np-nport/nP.js
+++ src/np-nport/nP.js
@@ -209,15 +209,11 @@
 		var frequencyList = global.fList, Ro = global.Ro;
 		var Zo = complex(Ro,0), Yo = Zo.inv(), two = complex(2,0), freqCount = 0, s11, s12, s13, s21, s22, s23, s31, s32, s33, sparsArray = [];
 		for (freqCount = 0; freqCount < frequencyList.length; freqCount++) {
-			s11 = complex(1/3,0);
-			s12 = complex(0.00001,0.000001);//complex(2/3,0);
-			s13 = s11; //s12;
-			s21 = s12;
-			s22 = s11;
-			s23 = s12;
-			s31 = s11; //s12;
-			s32 = s12;
-			s33 = s11;
+
+			s11 = complex(1/3,0); s12 = complex(2/3,0); s13 = complex(-1,0);
+			s21 = complex(2/3,0); s22 = complex(1/3,0); s23 = complex(-1,0);
+			s31 = complex(-1,0)  ; s32 = complex(-1,0) ; s33 = complex(0,0);
+
 			sparsArray[freqCount] =	[frequencyList[freqCount],s11, s12, s13, s21, s22, s23, s31, s32, s33];
 		}	
 		seriesTee.setspars(sparsArray);
